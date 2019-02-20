14235a44be44a2a972d36fe804be8d61c15248a3
SreelakshmiSrikanth <47592313+SreelakshmiSrikanth@users.noreply.github.com>
2019-02-20 14:04:25 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
- int main()
+ 
+int main()
 {
-   int a=10,b=10;
-   int c;  
-   c = a + b; 
+   int a=10, b=10, c;
+   
+  
+   
+  
+   c = a + b;
+   
    printf("%d", c);
+   
    return 0;
 }
