fa75c233185ad9bf63f4185036d1f60a8de99874
SreelakshmiSrikanth <47592313+SreelakshmiSrikanth@users.noreply.github.com>
2019-02-20 12:44:39 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -4,12 +4,11 @@ int main()
 {
    int a, b, c;
    
-   printf("Enter two numbers to add\n");
    scanf("%d%d", &a, &b);
    
    c = a + b;
    
-   printf("Sum of the numbers = %d\n", c);
+   printf("%d", c);
    
    return 0;
 }
